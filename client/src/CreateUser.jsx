e41afc986e931cfdc0c27c1fda2a030fe2fc2668
Arisha-Akbar <arishaakbar713@gmail.com>
2026-02-10 17:16:45 +0500

    implement user creation functionality with form handling in CreateUser component

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+
+  const navigate = useNavigate();
+
+  const Submit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/createUser", { name, email, age })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-      <div className="w-50 bg-white rounded p-3">
-        <form>
+      <div className="w-50 bg- white rounded p-3">
+        <form onSubmit={Submit}>
           <h2>Add user</h2>
           <div className="mb-2">
             <label htmlFor="">Name</label>
@@ -12,6 +31,7 @@ function CreateUser() {
               type="text"
               className="form-control"
               placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -20,6 +40,7 @@ function CreateUser() {
               type="email"
               className="form-control"
               placeholder="Enter Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -28,6 +49,7 @@ function CreateUser() {
               type="text"
               className="form-control"
               placeholder="Enter Age"
+              onChange={(e) => setAge(e.target.value)}
             />
           </div>
           <button className="btn btn-success">Submit</button>
